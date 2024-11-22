aa8f85e03275bfd12687985b0452fe58ecd70ccd
lucasilvafe <luksilvafe@gmail.com>
2024-11-22 18:45:05 -0300

    att de sexta-feira 22-11-2024 18:45

--- src/assets/js/acessibilidade.js
+++ src/assets/js/acessibilidade.js
@@ -1,7 +1,10 @@
 const acessibilidade = document.querySelector(".btn-accessibilidade");
 const menu = document.querySelector(".menu-acessibilidade");
-
 const botoes = document.querySelector(".botoes-acessibilidade-menu")
+const ativarSliderTexto = document.getElementById("slider-texto");
+const slider = document.getElementById("tamanhoTexto");
+const divSliderTexto = document.getElementById("slider-texto");
+const div = document.getElementById("range-text-size");
 
 
 acessibilidade.addEventListener("click", () =>
@@ -10,8 +13,9 @@ acessibilidade.addEventListener("click", () =>
 acessibilidade.addEventListener("click", () =>
     botoes.classList.toggle("active"));
 
-const ativarSliderTexto = document.getElementById("slider-texto");
-const slider = document.getElementById("tamanhoTexto");
 
 ativarSliderTexto.addEventListener("click", () =>
     slider.classList.toggle("active"));
+
+divSliderTexto.addEventListener("click", () =>
+    div.classList.toggle("active"));
